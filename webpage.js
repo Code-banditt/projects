345c688e42f9402ae726066a189f4f46cf3ec4b7
Code-banditt <157356175+Code-banditt@users.noreply.github.com>
2024-01-29 20:50:44 +0100

    Add files via upload

--- webpage.js
+++ webpage.js
@@ -1,7 +1,9 @@
 ///////webpage
 const buttonSwitch = document.querySelector(".nav-icon");
 const navigator = document.querySelector(".main-header");
-
+const stickynav = document.querySelector(".sticky-content");
+//all section
+const allsect = document.querySelectorAll("section");
 //
 const advertOne = document.querySelector(".Advert-section");
 
@@ -35,3 +37,43 @@ arrowUp.addEventListener("click", function (e) {
   console.log("currentScroll(X/Y)", window.pageXOffset, window.pageYOffset);
   navigator.scrollIntoView({ behavior: "smooth" });
 });
+
+//revealsection
+
+const revealsect = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) return;
+  entry.target.classList.add("sum2");
+};
+
+const sectReaveal = new IntersectionObserver(revealsect, {
+  root: null,
+  threshold: 0.15,
+});
+
+allsect.forEach(function (section) {
+  sectReaveal.observe(section);
+  section.classList.add("reveal");
+  section.classList.add("sum3");
+});
+
+//sticky
+
+const navheight = stickynav.getBoundingClientRect().height;
+
+console.log(navheight);
+
+const stickTO = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+  if (entry.isIntersecting) stickynav.classList.add("sum3");
+  else stickynav.classList.remove("sum3");
+};
+
+const interStick = new IntersectionObserver(stickTO, {
+  root: null,
+  threshold: 0.15,
+  rootMargin: `-${navheight}px`,
+});
+interStick.observe(navigator);
